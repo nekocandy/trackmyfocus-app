9006f6b07dbb14898273c89edb17e1db167cd8fd
Piyush <bhangalepiyush@gmail.com>
2022-12-17 20:35:55 +0530

    feat: add icons

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useState } from "react";
+import { Bulb, HandTwoFingers } from "tabler-icons-react";
 
 export default function HomePage(): ReactElement {
 	const [loading, setLoading] = useState(false);
@@ -20,10 +21,16 @@ export default function HomePage(): ReactElement {
 		return (
 			<div className="h-full flex items-center justify-center">
 				<div className="w-full grid grid-cols-2 gap-4">
-					<button className="text-orange-400 bg-orange-400/20 hover:(bg-orange-500/30) px-4 py-1 rounded">
+					<button className="flex items-center justify-center gap-1  text-orange-400 bg-orange-400/20 hover:(bg-orange-500/30) px-4 py-1 rounded">
+						<Bulb size={20} strokeWidth={2} className="text-orange-400" />
 						Prompt Me
 					</button>
-					<button className="text-teal-400 bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-1 rounded">
+					<button className="font-bol flex items-center justify-center gap-1  text-teal-400 bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-1 rounded">
+						<HandTwoFingers
+							size={20}
+							strokeWidth={2}
+							className="text-teal-400"
+						/>
 						Tap Button
 					</button>
 				</div>
