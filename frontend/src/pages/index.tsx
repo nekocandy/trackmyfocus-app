f9b6cf1e4b82edcd964454059f06a383e4c90cc2
Piyush <bhangalepiyush@gmail.com>
2022-12-17 20:30:09 +0530

    deps: add icons

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -20,12 +20,11 @@ export default function HomePage(): ReactElement {
 		return (
 			<div className="h-full flex items-center justify-center">
 				<div className="w-full grid grid-cols-2 gap-4">
-					<button className="text-teal-400 bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-1 rounded">
+					<button className="text-orange-400 bg-orange-400/20 hover:(bg-orange-500/30) px-4 py-1 rounded">
 						Prompt Me
 					</button>
-
-					<button className="text-orange-400 bg-orange-400/20 hover:(bg-orange-500/30) px-4 py-1 rounded">
-						Tap Me
+					<button className="text-teal-400 bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-1 rounded">
+						Tap Button
 					</button>
 				</div>
 			</div>
@@ -33,7 +32,10 @@ export default function HomePage(): ReactElement {
 	} else {
 		return (
 			<div className="h-full flex items-center justify-center">
-				<button onClick={loginWithBrowserClicked} className="w-full bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-2 rounded">
+				<button
+					onClick={loginWithBrowserClicked}
+					className="w-full bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-2 rounded"
+				>
 					Login With Browser
 				</button>
 			</div>
