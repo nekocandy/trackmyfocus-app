9da7eb70e8ce765e676bf79296779134d3dd83c4
Piyush <bhangalepiyush@gmail.com>
2022-12-18 12:55:57 +0530

    frontend: auth

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,18 +1,52 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { Bulb, HandTwoFingers } from "tabler-icons-react";
+import queryString from "query-string";
+import { useNavigate } from "react-router";
+
+interface Window {
+	bridge: any;
+}
+
+declare let window: Window;
 
 export default function HomePage(): ReactElement {
+	const [registered, setRegistered] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [signedIn, setSignedIn] = useState(false);
+	const [token, setToken] = useState<string | null>(null);
+	const navigate = useNavigate();
 
 	const wait = (seconds: number) =>
 		new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
+	const listener = () => {
+		console.log("hello from");
+
+		if (registered) {
+			console.log("tried registering again");
+			return;
+		}
+
+		setRegistered(true);
+
+		window.bridge.setToken((event: any, token: string) => {
+			console.log({ event, token });
+			const params = queryString.parse(token.split("?")[1]);
+
+			localStorage.setItem("token", params["token"] as string);
+			setToken(params["token"] as string);
+			setLoading(false);
+			setSignedIn(true);
+		});
+	};
+
+	useEffect(() => {
+		listener();
+	}, []);
+
 	const loginWithBrowserClicked = async () => {
 		setLoading(true);
-		await wait(1);
-		setLoading(false);
-		setSignedIn(true);
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
 	};
 
 	if (loading) {
@@ -39,12 +73,14 @@ export default function HomePage(): ReactElement {
 	} else {
 		return (
 			<div className="h-full flex items-center justify-center">
-				<button
+				<a
 					onClick={loginWithBrowserClicked}
-					className="w-full bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-2 rounded"
+					href="http://localhost:4432/auth/token"
+					target="_blank"
+					className="inline-flex w-full bg-teal-500/20 hover:(bg-teal-500/30) px-4 py-2 rounded"
 				>
 					Login With Browser
-				</button>
+				</a>
 			</div>
 		);
 	}
