051868687e8757893e88d4afda8c1ba6d83fb1e2
Piyush <bhangalepiyush@gmail.com>
2022-12-18 11:31:53 +0530

    update variable name

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
 	app.quit();
 } else {
-	app.on("second-instance", (event, commandLine, workingDirectory) => {
+	app.on("second-instance", (event, commandLine: string[]) => {
 		if (mainWindow) {
 			if (mainWindow.isMinimized()) mainWindow.restore();
 			mainWindow.focus();
